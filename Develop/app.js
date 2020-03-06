0d8e64d7a36519fcc743bf8f0b157ed314304f36
Matthew Bell <matthewbell1030@gmail.com>
2020-03-06 06:38:37 -0800

    Updated function for prompts in 'app.js', including adding a switch case for the prompts based on employee jobTitle as well as reordering the prompts to make more sense.  I then chained the function that will create employee 'objects' based on the user's input to run after the user has answered all prompts.

--- Develop/app.js
+++ Develop/app.js
@@ -13,90 +13,98 @@ const render = require("./lib/htmlRenderer");
 ​
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-inquirer.prompt([
-    // Prompts requesting info about Team Manager
-    {
-      type: "input",
-      name: "nameManager",
-      message: "What is the Team Manager's name?"
-    },
-    {
-      type: "input",
-      name: "idManager",
-      message: "What is the Team Manager's id?"
-    },
-    {
-      type: "input",
-      name: "emailManager",
-      message: "What is the Team Manager's emil?"
-    },
-    {
-      type: "input",
-      name: "officeManager",
-      message: "What is the Team Manager's office number?"
-    },
-    // Prompt requesting info about number of Team Members
-    {
-      type: "input",
-      name: "teamCount",
-      message: "How many employees will be assigned to this project?"
-    },
-    // Prompts requesting info about Team Member details
-    {
-      type: "input",
-      name: "name",
-      message: "What is the employee's name?"
-    },
-    {
-      type: "checkbox",
-      message: "What is this employee's job title?",
-      name: "jobTitle",
-      choices: [
-        "Engineer", 
-        "Intern", 
-      ]
-    },
-    {
+function userPrompts() {
+    inquirer.prompt([
+        // Prompt requesting info about number of Team Members.
+        {
         type: "input",
-        name: "id",
-        message: "What is this employee's id?"
-      },
-    {
+        name: "teamCount",
+        message: "How many employees will be assigned to this project?"
+        },
+        // Prompts requesting info about common Team Member details.
+        {
         type: "input",
-        name: "gitHub",
-        message: "What is this Engineer's gitHub?"
-      },
-    {
-        type: "input",
-        name: "school",
-        message: "What is this Intern's school?"
-      }
-      // After user has answered all prompts...
-  ]).then(function(data) {
-        // Create objects for each team member
+        name: "name",
+        message: "What is the employee's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is this employee's id?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is this employee's email address?"
+        },
+        {
+        type: "checkbox",
+        message: "What is this employee's job title?",
+        name: "jobTitle",
+        choices: [
+            "Manager",
+            "Engineer", 
+            "Intern"
+        ]
+        }
+    // Based on employee's jobTitle, prompt the following in each case.
+    ]).then(function() {
+        switch (jobTitle) {
+            case "Manager":
+                // Prompt requesting info about case-specific Team Member details.
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "officeManager",
+                        message: "What is this Team Manager's office number?"
+                    }]);
+            case "Engineer":
+                // Prompt requesting info about case-specific Team Member details.
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "gitHub",
+                        message: "What is this Engineer's gitHub?"
+                    }]);
+            case "Intern":
+                // Prompt requesting info about case-specific Team Member details.
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "school",
+                        message: "What is this Intern's school?"
+                    }]);
+        };
+    });
+};
+// When program first runs, user will be prompted with above questions.
+// After user has answered all prompts, then...
+userPrompts().then(function(data) {
+        // Create objects for each team member.
     const employees = "";
         console.log("employees: \n" + employees);
-  });
-​
+});
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 render(employees);
-​
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work!```
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+​
+
--- README.md
+++ README.md
@@ -26,3 +26,5 @@ Progress:
 2020.03.06_0100 - Initial commit. Set up folder structure and imported initial starting point from "https://ucsd.bootcampcontent.com/UCSD-Coding-Bootcamp/UCSD-SD-FSF-PT-12-2019-U-C/tree/master/10-OOP/02-Homework".
 
 2020.03.06_0612 - Corrected resources used section in 'readme.md'.  Added first pass prompts, and began the .then function that runs after prompts have been answered by the user, and the render function in 'app.js'.  I also added comments to 'app.js'.
+
+2020.03.06_0635 - Updated function for prompts in 'app.js', including adding a switch case for the prompts based on employee jobTitle as well as reordering the prompts to make more sense.  I then chained the function that will create employee 'objects' based on the user's input to run after the user has answered all prompts.
